84f11cffc5c72ebd450730bc27de0f0b350541ec
OsielTreto <treto.cedillo@itsmante.edu.mx>
2020-05-11 20:25:01 -0500

    Commit

--- src/app/pages/costos-agregar/costos-agregar.page.ts
+++ src/app/pages/costos-agregar/costos-agregar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { ModalController } from "@ionic/angular";
+import { CostosService } from '../../services/costos.service';
 
 @Component({
   selector: "app-costos-agregar",
@@ -7,21 +8,43 @@ import { ModalController } from "@ionic/angular";
   styleUrls: ["./costos-agregar.page.scss"],
 })
 export class CostosAgregarPage implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+  constructor(private modalCtrl: ModalController,
+    public costosService: CostosService,
+  ) { }
 
-  @Input() semilla;
-  @Input() descripcion;
+  datos = {
+    idSemillas: '',
+    precio: '',
+    descripcion: ''
+  }
 
-  ngOnInit() {}
+
+  ngOnInit() { }
+
+  onSubmitCostos() {
+    this.costosService.insertCostos(this.datos).then(Response => {
+      let data = JSON.parse(Response.data);
+      let datosCostos = data.result;
+      if (datosCostos == 'success') {
+        console.log("Se ha insertado con exito");
+      } else {
+        console.log("No se ha insertado con exito");
+      }
+    });
+    this.salirSinArgumentos();
+  }
 
   salirSinArgumentos() {
     this.modalCtrl.dismiss();
   }
 
   salirConArgumentos() {
+    console.log("Form submit");
+    console.log(this.datos);
     this.modalCtrl.dismiss({
-      semilla: "semilla desde hijo",
-      descripcion: "descripcion desde hijo",
+      semilla: this.datos.idSemillas,
+      precio: this.datos.precio,
+      descripcion: this.datos.descripcion,
     });
   }
 }
